00689f35fa28eba5986e4898401e18d71d81bae0
Kevin Lu <69993704+kevlu8@users.noreply.github.com>
2026-03-01 12:46:31 -0800

    Add pawn correction history (#12)
    ```
    Elo   | 24.14 +- 13.94 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | 3.04 (-2.25, 2.89) [0.00, 10.00]
    Games | N: 1384 W: 489 L: 393 D: 502
    Penta | [56, 113, 273, 179, 71]
    ```
    https://ob.int0x80.ca/test/425/
    
    Bench: 1098863

--- src/history.cpp
+++ src/history.cpp
@@ -4,3 +4,19 @@ void History::update_history(Board &board, Move move, int bonus) {
 	int cbonus = std::clamp(bonus, -MAX_HISTORY, MAX_HISTORY);
 	history[board.side][move.src()][move.dst()] += cbonus - history[board.side][move.src()][move.dst()] * abs(cbonus) / MAX_HISTORY;
 }
+
+void History::update_corrhist(Board &board, int bonus) {
+	auto update_entry = [=](int &entry) {
+		int update = std::clamp(bonus, -MAX_CORRHIST / 4, MAX_CORRHIST / 4);
+		entry += update - entry * abs(update) / MAX_CORRHIST;
+	};
+
+	update_entry(corrhist_ps[board.side][board.pawn_hash() % CORRHIST_SZ]);
+}
+
+int History::get_correction(Board &board) {
+	int corr = 0;
+	corr += 128 * corrhist_ps[board.side][board.pawn_hash() % CORRHIST_SZ];
+
+	return corr / 2048;
+}
